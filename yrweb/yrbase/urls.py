cd3e9c23d226a67931445a6fe3b9f5be4aa13c6e
Yu-Yu Huang <hyuyu1544@gmail.com>
2020-10-12 17:12:21 +0800

    fix: dev: change index page

--- yrweb/yrbase/urls.py
+++ yrweb/yrbase/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # path('<int:id>/', views.newsfunc, name='newsfunc'), # `yrbase/5/`, 5 is an input from `newsfunc` in views.py
 ]

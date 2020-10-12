cf76b865ac636317757f99c23e5dfa8e634f88d9
Yu-Yu Huang <hyuyu1544@gmail.com>
2020-10-12 16:24:06 +0800

    add: dev: add a simple index page

--- yrweb/yrbase/views.py
+++ yrweb/yrbase/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
 
 def index(request):
     """Index"""
-    return HttpResponse("Hello World")
+    return render(request, 'yrbase/index.html')
